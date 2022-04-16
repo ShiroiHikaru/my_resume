8b250776d678c49b6255649e834aa5d95b668490
ShiroiHikaru <asd95121@naver.com>
2022-04-17 01:58:36 +0900

    git add .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+// eslint-disable 
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let pinktext = {color : '#FF6FA0', fontWeight : 'bold'};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className='container'>
+        <div className='main-text'>
+          <h3>Metamorphosis And Humble</h3>
+          <h1 style={pinktext}>Hee Yeon<br/><span className='akaName'>a.k.a HIKARU</span></h1>
+          <div className='intro-text'>
+            <p className='intro-border'>매일매일이 <span style={pinktext}>색다른 메타몽</span>처럼,<br/>하지만 항상 겸손한 마음가짐으로</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
