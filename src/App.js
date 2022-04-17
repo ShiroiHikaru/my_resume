00ccdea28ae19d3559c0b73c39b214140fa5d237
ShiroiHikaru <asd95121@naver.com>
2022-04-17 13:10:04 +0900

    icon add

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import mainProfile from '../src/images/main_profile.png'
+import mainProfile from '../src/images/main_profile.png';
+import profileIcon from '../src/images/profileicon.svg';
+import github from '../src/images/github.svg';
 function App() {
   let pinktext = {color : '#FF6FA0', fontWeight : 'bold'};
 
   return (
     <div className="App">
       <main className='container'>
-        <div className='main-text'>
-          <h3>Metamorphosis And Humble</h3>
-          <h1 style={pinktext}>Hee Yeon<br/><span className='akaName'>a.k.a HIKARU</span></h1>
-          <div className='intro-text'>
-            <p className='intro-border'>매일매일이 <span style={pinktext}>색다른 메타몽</span>처럼,<br/>하지만 항상 겸손한 마음가짐으로</p>
-          </div>
-        </div>
-        <div className='mainProfile'>
-            <img src={mainProfile}/>
-        </div>
+        <section className='profile-wrap'>
+           <div className='profile'>
+              <div className='main-text'>
+                  <h3>Metamorphosis And Humble</h3>
+                  <h1 style={pinktext}>Hee Yeon<br/><span className='akaName'>a.k.a HIKARU</span></h1>
+                  <div className='intro-text'>
+                    <p className='intro-border'>매일매일이 <span style={pinktext}>색다른 메타몽</span>처럼,<br/>하지만 항상 겸손한 마음가짐으로</p>
+                  </div>
+                </div>
+                <div className='mainProfile'>
+                    <img src={mainProfile}/>
+                </div>
+           </div>
+            <div className='categori'>
+                <ul>
+                  <li>Profile</li>
+                  <li>Publishing</li>
+                  <li>Web / Landing</li>
+                  <li>Graphic</li>
+                  <li>Social</li>
+                  <li>Youtube</li>
+                </ul>
+            </div>
+        </section>
       </main>
     </div>
   );
